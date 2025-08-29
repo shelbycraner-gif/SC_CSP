34c159fd82d2b3f67908a4a3ac2afdfe176f5600
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-08-29 14:42:46 -0600

    variable notes

---
+++ practices/variables_practice.py
@@ -0,0 +1,16 @@
+print ("welcome to variables notes!")
+
+#examples of variables
+school = "UCAS"
+single_digit = 4
+how_much = 10
+
+print(how_much)
+
+how_much = 9999999999999999999
+
+print(how_much)
+
+name = input("what is your name?") 
+
+print("Hello,name")
