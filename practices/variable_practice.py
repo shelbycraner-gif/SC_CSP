e1afa2a507e1d2a85de713618cd444e9190b597b
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-02 14:35:29 -0600

    finished assignment

---
+++ practices/variable_practice.py
@@ -0,0 +1,23 @@
+# S.C 7th Variable Porject
+
+your_name = ("Shelby")
+school_name = ("UCAS")
+number_between_1_and_10 = 7
+number_between_10_and_100  = 500
+year = 2025
+your_eye_color = ("brown")
+What_you_had_for_breakfast = ("nothing")
+your_age = 14
+your_favorite_color = ("dark purple")
+your_favorite_subject_in_school = ("world civ")
+
+print(your_name)
+print(school_name)
+print(number_between_1_and_10)
+print(number_between_10_and_100)
+print(year)
+print(your_eye_color)
+print(What_you_had_for_breakfast)
+print(your_age)
+print(your_favorite_color)
+print(your_favorite_subject_in_school)
