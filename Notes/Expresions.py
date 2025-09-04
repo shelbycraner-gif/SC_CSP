888084cdcfa91d61dd1be4313e28a93eb0a15c7e
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-04 14:40:47 -0600

    Expressions

---
+++ Notes/Expresions.py
@@ -0,0 +1,43 @@
+# VL 7th Expression Notes
+
+# What is an algorithm
+    # a set of instructions to complete a task
+name = input("What is your name:\n")
+print("Hello", name)
+
+# 2 apples
+apples =  12
+friends = 5
+print("Each friend gets", apples/friends, "apples!")
+# List steps in an algorithm
+
+# Average age of a group of 4 people
+friend1 = 14
+friend2 = 13
+friend3 = 14
+friend4 = 15
+print("Average age of friends", (friend1+friend2+friend3+friend4)/4)
+    # 1) Declare variables (get info thats needed)
+    # 2) Preform any equations (do the equations) --> make the things different
+    # 3) Display results (print the results)
+
+# List ALL different Mathematical operators (symbol+tell what it does)
+num1 = 12
+num2 = 7
+
+print("Addition(+): ", num1+num2)
+print("Subtraction(-): ", num1-num2)
+print("Multiplication(*): ", num1*num2)
+print("division(/): ", num1/num2)
+print("Exponents(**): ", num1**num2)
+print("Integer Division(//): ", num1//num2)
+print("Modulo(%): ", num1%num2)
+# Integer dividing only gives a whole number 
+# Floats are numbers with decimals
+# Modulo is the left over amount of your division i.e your remainder
+
+# List ALL of the different assignment operators (symbols+what it does)
+    #
+
+# Why are expressions so important in programming?
+    #
