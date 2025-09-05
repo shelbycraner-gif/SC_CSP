919c616ea0864bcc77956048c0f0d8bc44c7682a
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-05 14:42:10 -0600

    Expression Practice

---
+++ practices/budget_calculator.py
@@ -0,0 +1,6 @@
+# S.C 7th Budget Calculator
+
+# income, rent, utilities, groceries, transportation
+# Savings = 10% income (income*.1)
+# spend = income - (rent + utiltilties + groceries + transportation)
+# percent_rent = rent/income*100
