456b2086359916ea5bd45a20432a4a54b131ba44
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-09 13:49:11 -0600

    strings

--- Notes/string.py
+++ Notes/string.py
@@ -1,2 +1,36 @@
 # S.C 7th String Notes
 
+#What makes something a string?
+    # A set of symbols held together by a set of quotation marks "" or ''
+first_name = input("What is your First name:\n").strip().lower().title()
+
+last_name = input("What is your Last name:\n").strip().lower().title()
+
+sentence = 'The quick brown fox jumps over the lazy dog'
+
+print("Welcome to my program!")
+
+print(sentence.strip())
+
+full_name = first_name +" "+ last_name
+print("welcome to my program")
+
+
+#Why do we have strings?
+    # 
+#What is debugging?
+    # 
+#How do you debug the different types of errors?
+    # 
+#Describe what each of the methods below does:
+    # 
+#find()
+    # 
+#concatenate (add)
+    # putting two strings together to make one string
+#upper()
+    # whatever user types in makes it lowercase
+#lower()
+    # whatever user types in makes it uppercase
+#strip()
+    # gets rid of extra spaces in beggining and end
