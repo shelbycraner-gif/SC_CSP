01e24aa1d305f47e6a062015b54c65df8913348a
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-10 14:33:02 -0600

    Name dec. and silly sent. added

---
+++ practices/name_decorator.py
@@ -0,0 +1,4 @@
+# SC 7th Name Decorator
+
+name = input("What is your name?\n").capitalize().strip()
+print(f"Hello <<{name}>>" + "!")
---
+++ practices/silly_sentences.py
@@ -0,0 +1,9 @@
+# SC 7th Silly Sentences
+
+verb1 = input("Give me a verb!\n")
+Adjetive1 = input("Give me an adjetive\n")
+verb2 = input("Give me another verb, please!\n")
+Noun = input("Give me a noun!\n")
+Adjetive2 = input("Please give me another adjetive\n")
+
+print(f"You were {verb1} down a {Adjetive1} road and then you saw a cat {verb2} along the sidewalk! You saw a {Adjetive2} {Noun} and you ran to it because you were being chased by that cat!")
