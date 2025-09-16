005ed41f4fcd7724effa2d0ce609bb5159819312
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-16 14:42:02 -0600

    made update hello finished

---
+++ practices/update_hello.py
@@ -0,0 +1,12 @@
+# S.C 7th Update Hello World
+
+def hello(username):
+    print(f"Hello {username}")
+    return name
+
+name = input("What is your name?\n")
+hello("Camry")
+hello("Maleri")
+hello("Carter")
+hello("Carl")
+hello(name)
