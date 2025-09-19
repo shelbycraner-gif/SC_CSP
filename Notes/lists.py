5bbffcb9ccbaa1acea278c3ae8824c601f19a1bb
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-19 14:29:48 -0600

    Finished time of day assignment

---
+++ Notes/lists.py
@@ -0,0 +1,5 @@
+# SC 7th Lists Notes
+
+siblings = ["Camry", "Carter", "Maleri", "Shelby", ["Ezra", "Claudia", "Felichia", "Andrew", "Tyler", "Natalie", "Matthew", "Kayson"]]
+
+# structure: lists are surrounded by brackets and are all seperated from eachother from commas
--- practices/time_day.py
+++ practices/time_day.py
@@ -1,2 +1,15 @@
 # SC 7th Time of Day
 
+import datetime
+
+now = int(input("Insert the current time for you in military time: \n"))
+now_is = datetime.datetime.now()
+
+if now >= 1 and now < 11:
+    print("Good morning!")
+elif now >= 1200 and now < 1730:
+    print("Good Afternoon!")
+elif now >= 1731 and now < 23:
+    print("Good evening")
+else:
+    print("Are you dumb? That isnt a time idiot?")
