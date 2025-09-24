c82b2f76fef35885297427e6cf11def77c4910f0
shelbycraner-gif <shelby.craner@ucas-edu.net>
2025-09-24 14:40:48 -0600

    Started gameplan teamplan

---
+++ practices/team_game.py
@@ -0,0 +1,7 @@
+# SC 7th Team Game, Text Based Adventure
+
+# Danial-does monster incounter
+# Shelby-does the journey(-ies)
+# Elena-does beginning dialogue(wizards)
+# Addie-does endings (dies, survives, finds wizards things, success, etc)
+
