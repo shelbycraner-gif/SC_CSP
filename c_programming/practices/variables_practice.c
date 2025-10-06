7b330c52bdd5399b3ccb4294a574c30d0af53480
Cat <shelby.craner@ucas-edu.net>
2025-10-06 23:47:43 +0000

    Finished variables practice

--- c_programming/practices/variables_practice.c
+++ c_programming/practices/variables_practice.c
@@ -2,5 +2,24 @@
 #include <stdio.h>
 
 int main(void){
-    printf("")
+    char name = "Shelby Craner"
+    char school = "Utah County Academy of Sciences"
+    int number = 7
+    int year = 2025
+    float gpa = 3.558
+    char eye = "Medium Brown"
+    char breakfast = "nothing"
+    int age = 14
+    char color = "Lavender Purple"
+    char subject = "World Civilizations"
+    printf("My name is %c", name)
+    printf("I go to %c", school)
+    printf("A number between 1 and 10 is %d", number)
+    printf("It is %d currently", year)
+    printf("A GPA is %f", gpa)
+    printf("My eye color is a %c", eye)
+    printf("I ate %c for breakfast today", breakfast)
+    printf("I am %d years old", age)
+    printf("My favorite color is %c", color)
+    printf("My favorite subject in school is %c", subject)
 }
