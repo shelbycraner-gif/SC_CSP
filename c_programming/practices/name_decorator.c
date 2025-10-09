c0e8e6b9cdbf4c348e8e05246337f7da08daac08
shelbycraner <shelby.craner@ucas-edu.net>
2025-10-09 14:41:58 -0600

    name decorator c

---
+++ c_programming/practices/name_decorator.c
@@ -0,0 +1,13 @@
+//SC 7th Name Decorator
+#include <stdio.h>
+
+int main(void){
+    char name[15];
+    char concat_nate[] = "<<>>";
+    printf("What is your name?");
+    scanf("%s\n", name);
+    strcat(
+    printf("Hello <<%s>>", name);
+
+    return 0;
+}
