e0c6c8c46714a053341cbf21615656de13fcd628
Cat <shelby.craner@ucas-edu.net>
2025-10-14 01:03:52 +0000

    did silly sentences

--- c_programming/practices/silly_sentences.c
+++ c_programming/practices/silly_sentences.c
@@ -1,3 +1,21 @@
 //SC 7th Silly Sentences
 #include <stdio.h>
 #include <string.h>
+
+int main(void){
+    char noun[20];
+    char verb[20];
+    char verb2[20];
+    char adjective[20];
+    printf("Give me a noun: \n");
+    scanf("%s", noun);
+    printf("Give me a verb: \n");
+    scanf("%s", verb);
+    printf("Give me an adjective: \n");
+    scanf("%s", adjective);
+    printf("Give me another verb: \n");
+    scanf("%s", verb2);
+
+    printf("The %s was %s around the area. It saw a %s bug and decided to %s with it.\n", noun, verb, adjective, verb2);
+    return 0;
+}
