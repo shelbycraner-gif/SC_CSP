1d1c3725367552c2a9729a7aa2f7362bcdf10a1d
Cat <shelby.craner@ucas-edu.net>
2025-10-14 00:09:26 +0000

    fixed fizzbuzz

--- c_programming/practices/fizzbuzz.c
+++ c_programming/practices/fizzbuzz.c
@@ -2,20 +2,18 @@
 #include <stdio.h>
 
 int main(void){
-    int num = 1;
 
-    while(num < 51){
+    for (int x = 1; x <= 50; x++){
     
-    if (num / 3 == 0 && num / 5 == 0){
-         printf("FizzBuzz!\n");
-    }else if (num / 5 == 0){
-        printf("Buzz!\n");
-    }else if (num / 3 == 0){
-        printf("Fizz!\n");
-    }else{
-        printf("%s \n");
-    }
-    num++;
+        if (x % 3 == 0 && x % 5 == 0){
+            printf("FizzBuzz!\n", x);
+        }else if (x % 5 == 0){
+            printf("Buzz!\n", x);
+        }else if (x % 3 == 0){
+            printf("Fizz!\n", x);
+        }else{
+            printf("%d\n", x);
+        }
     }
        
     return 0;
