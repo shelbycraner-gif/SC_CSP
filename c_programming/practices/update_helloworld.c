963a76b43c011750e64236591026fd7472bdf9bd
shelbycraner <shelby.craner@ucas-edu.net>
2025-10-15 14:29:13 -0600

    made update hello world, strated budget calculator

---
+++ c_programming/practices/update_helloworld.c
@@ -0,0 +1,13 @@
+//SC 7th Update Hello World
+#include <stdio.h>
+void hello(char* name){
+    printf("Hello! %s!\n", name);
+}
+int main(void){
+    hello("Camry");
+    hello("Carter");
+    hello("Father");
+    hello("Mother");
+    hello("Maleri");
+   return 0;
+}
